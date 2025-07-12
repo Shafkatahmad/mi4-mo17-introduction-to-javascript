9991eb01131a9407169d47684f9a884637951ee8
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-12 22:57:45 +0600

    variable types

---
+++ types.js
@@ -0,0 +1,17 @@
+// String type
+var name = "Sadar Uddin";
+console.log(name);
+var address = "Adar Killa bandorban with some katabon";
+var country = 'Babgladesh';
+
+// boolean type
+var isRich = true;
+console.log(isRich)
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+// number types
+var price = 22;
+console.log(typeof price);
