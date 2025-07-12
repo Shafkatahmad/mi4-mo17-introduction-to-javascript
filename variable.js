0f53705c8d44c3ca95af85de744deb32f0f18089
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-12 22:40:38 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,13 @@
+/**
+ * VARIABLE ==> vary + able
+ * 5 things to declare a variable
+ */
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+money = 505;
+var age;
+console.log(age);
+console.log(money);
+console.log(price);
